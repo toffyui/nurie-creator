c2cf8f432e613f3bd89192d94d294c877c687439
YuikoIto <toffywing@gmail.com>
2021-02-28 15:07:39 +0900

    fix::bug:fix dependency

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,8 +49,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
