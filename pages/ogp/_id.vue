44b68c8a57285330ff465374b4f1c4820c79ba16
YuikoIto <toffywing@gmail.com>
2021-02-28 23:46:30 +0900

    fix::bug:meta

--- pages/ogp/_id.vue
+++ pages/ogp/_id.vue
@@ -5,26 +5,26 @@
 </template>
 <script>
 export default {
-  //   data() {
-  //     return {
-  //       meta: {
-  //         url: `https://nurie-maker.com/ogp/?id=${this.$route.query.id}`,
-  //         image: `https://nurie.s3-ap-northeast-1.amazonaws.com/ogpimg/${this.$route.query.id}.jpg`,
-  //       },
-  //     }
-  //   },
+  data() {
+    return {
+      meta: {
+        url: `https://nurie-maker.com/ogp/?id=${this.$route.query.id}`,
+        image: `https://nurie.s3-ap-northeast-1.amazonaws.com/ogpimg/${this.$route.query.id}.jpg`,
+      },
+    }
+  },
   head() {
     return {
       meta: [
         {
           hid: 'og:url',
           property: 'og:url',
-          content: `https://nurie-maker.com/ogp/?id=${this.$route.query.id}`,
+          content: this.meta.url,
         },
         {
           hid: 'og:image',
           property: 'og:image',
-          content: `https://nurie.s3-ap-northeast-1.amazonaws.com/ogpimg/${this.$route.query.id}.jpg`,
+          content: this.meta.image,
         },
       ],
     }
