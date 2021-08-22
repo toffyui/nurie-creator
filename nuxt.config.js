60e6ae6b3fee23b9ac076e28b198dc92e8167ad4
yuikoito <yuiko.dev@gmail.com>
2021-08-22 19:18:45 +0900

    delete unuse modules

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,59 @@
-require('dotenv').config()
-const { API_BASE_URL } = process.env
+require("dotenv").config();
+const { API_BASE_URL } = process.env;
 export default {
   head: {
-    title: '塗り絵ツクール',
+    title: "塗り絵ツクール",
     htmlAttrs: {
-      lang: 'ja',
-      prefix: 'og: http://ogp.me/ns#',
+      lang: "ja",
+      prefix: "og: http://ogp.me/ns#"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: '塗り絵をかんたんに作れるサービスです',
+        hid: "description",
+        name: "description",
+        content: "塗り絵をかんたんに作れるサービスです"
       },
       {
-        hid: 'og:site_name',
-        property: 'og:site_name',
-        content: '塗り絵ツクール',
+        hid: "og:site_name",
+        property: "og:site_name",
+        content: "塗り絵ツクール"
       },
-      { hid: 'og:type', property: 'og:type', content: 'article' },
+      { hid: "og:type", property: "og:type", content: "article" },
       {
-        hid: 'og:url',
-        property: 'og:url',
-        content: 'https://nurie-maker.com',
+        hid: "og:url",
+        property: "og:url",
+        content: "https://nurie-maker.com"
       },
-      { hid: 'og:title', property: 'og:title', content: '塗り絵ツクール' },
+      { hid: "og:title", property: "og:title", content: "塗り絵ツクール" },
       {
-        hid: 'og:description',
-        property: 'og:description',
-        content: '塗り絵をかんたんに作れるサービスです',
+        hid: "og:description",
+        property: "og:description",
+        content: "塗り絵をかんたんに作れるサービスです"
       },
       {
-        hid: 'og:image',
-        property: 'og:image',
-        content: 'http://ogp-builder.com/MkU6cw/https://nurie-maker.com',
+        hid: "og:image",
+        property: "og:image",
+        content: "http://ogp-builder.com/MkU6cw/https://nurie-maker.com"
       },
-      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: "twitter:card", content: "summary_large_image" },
       {
-        name: 'twitter:image',
-        content: 'http://ogp-builder.com/MkU6cw/https://nurie-maker.com',
+        name: "twitter:image",
+        content: "http://ogp-builder.com/MkU6cw/https://nurie-maker.com"
       },
-      { name: 'twitter:site', content: '@yui_active' },
+      { name: "twitter:site", content: "@yui_active" }
     ],
     link: [
       {
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: 'https://yuilog.xyz/wp-content/uploads/2021/03/favicon-32x32.png',
-      },
-    ],
+        rel: "icon",
+        type: "image/x-icon",
+        href: "https://yuilog.xyz/wp-content/uploads/2021/03/favicon-32x32.png"
+      }
+    ]
   },
 
-  target: 'static',
+  target: "static",
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
@@ -65,19 +65,14 @@ export default {
   components: true,
 
   env: {
-    API_BASE_URL,
+    API_BASE_URL
   },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-  ],
-
+  buildModules: ["@nuxtjs/tailwindcss"],
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: ['@nuxtjs/axios', '@nuxtjs/dotenv'],
+  modules: ["@nuxtjs/axios", "@nuxtjs/dotenv"],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-}
+  build: {}
+};
